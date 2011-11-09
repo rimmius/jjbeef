2164f8f032834ab97963e0854967710465c1f9ea
gUstehn <fredrikgustafsson84@gmail.com>
2011-11-09 13:45:53 +0100

    Changed peers to store valid peers

--- peers.erl
+++ peers.erl
@@ -1,36 +1,29 @@
 -module(peers).
--export([start/0, get_a_peer/1, insert_new_peers/4]).
+-export([start/0, get_a_peer/1, insert_new_peers/3, insert_valid_peer/3]).
 -export([init/0]).
 
 start() ->
     spawn(peers, init, []).
 init() ->
-    loop({[], []}).
+    loop([]).
 
-loop({Peers_nu, Peers_u}) ->
+loop(Peers_valid) ->
     receive
-	{req_peer, From} ->
-	    case fetch_a_peer({Peers_nu, Peers_u}) of
-		 {{Ip, Port, Sock}, Peers_new, Peers_unew} ->
-		    From ! {reply, {Ip, Port, Sock}},
-		    loop({Peers_new, Peers_unew})
-	    end;
-	{insert_peers, From, List_of_peers} ->
+	{insert_peer, From, Sock, Peer_id, Ip} ->
 	    From ! {reply, ok},
-	    loop({List_of_peers, Peers_u});
+	    loop([{Peer_id, Ip, Sock, handshaken}|Peers_valid]);
 	{send_handshake, From, {Host, Port, Info, Peer_id}} ->
-	    case gen_tcp:connect(Host, Port, [binary, {packet, 0}], 1000) of
+	    case gen_tcp:connect(Host, Port, [binary, {active, false},{packet, 0}], 1000) of
 		{ok, Sock} ->
-		    Info_hashed = list_to_binary(sha:sha1hash(Info)),
-		    Msg = list_to_binary([<<19>>,<<"BitTorrent Protocol">>, <<3,2,1,3,2,1,2,3>>, Info_hashed,list_to_binary(Peer_id)]),
-		    io:format("baba~n"),
+		    Msg = list_to_binary([<<19>>,<<"BitTorrent protocol">>, <<3,2,1,3,2,1,2,3>>,Info,list_to_binary(Peer_id)]),
 		    ok = gen_tcp:send(Sock, Msg),
+		    io:format("Sent handshake to peer from tracker~n"),
 		    From ! {reply, ok, Sock},
-		    loop({Peers_nu, Peers_u});
+		    loop(Peers_valid);
 		{error, Reason} ->
 		    io:format(Reason),
 		    From ! {error, Reason},
-		    loop({Peers_nu, Peers_u})
+		    loop(Peers_valid)
 	    end
     end.
 fetch_a_peer({[], [H|T]}) ->
@@ -45,25 +38,22 @@ get_a_peer(Pid) ->
 	{reply, Peer} ->
 	    Peer
     end.
-insert_new_peers(List_raw, Peers_pid, Info, Dl_pid) ->
+insert_new_peers(List_raw, Peers_pid, Dl_pid) ->
     List_of_peers = make_peer_list(List_raw, "", 1, []),
-    port_listener:start(12345, Dl_pid),
-    List_handshaken = handshake_all_peers(List_of_peers, Info, download_manager:get_my_id(Dl_pid), [], Peers_pid),
-    Peers_pid ! {insert_peers, self(), List_handshaken},
-    receive
-	{reply, Reply} ->
-	    Reply
-    end.
+    port_listener:start(12345, Dl_pid, Peers_pid),
+    Info_hash = download_manager:get_my_info_hash(Dl_pid),
+    My_id = download_manager:get_my_id(Dl_pid),
+    ok = handshake_all_peers(List_of_peers, Info_hash, My_id, [], Peers_pid, Dl_pid).
 
-handshake_all_peers([], _Info, _Peer_id, New_list, _Peers_pid) ->
-    New_list;
-handshake_all_peers([{H, Port}|T], Info, Peer_id, New_list, Peers_pid) ->
+handshake_all_peers([], _Info, _Peer_id, New_list, _Peers_pid, _Dl_pid) ->
+    ok;
+handshake_all_peers([{H, Port}|T], Info, Peer_id, New_list, Peers_pid, Dl_pid) ->
     io:format(H),
-    case send_handshake(H, Port, Info, Peer_id, Peers_pid) of
+    case send_handshake(H, Port, Info, Peer_id, Peers_pid, Dl_pid) of
 	error ->
-	    handshake_all_peers(T, Info, Peer_id, New_list, Peers_pid);
+	    handshake_all_peers(T, Info, Peer_id, New_list, Peers_pid, Dl_pid);
 	Sock ->
-	    handshake_all_peers(T,Info, Peer_id, [{H, Port, Sock}|New_list], Peers_pid)
+	    handshake_all_peers(T,Info, Peer_id, [{H, Port, Sock}|New_list], Peers_pid, Dl_pid)
     end.
 make_peer_list([], _Ip, _Byte, New_list) ->
     New_list;
@@ -82,21 +72,50 @@ convert_to_ip([H|T], New_list) ->
 	    convert_to_ip(T,integer_to_list(H) ++ "." ++ New_list)
     end.
 
-send_handshake(Host, Port, Info, Peer_id, Pid) ->
-    Pid ! {send_handshake, self(), {Host, Port, Info, Peer_id}},
+send_handshake(Host, Port, Info, Peer_id, Peers_pid, Dl_pid) ->
+    Peers_pid ! {send_handshake, self(), {Host, Port, Info, Peer_id}},
     receive
 	{reply, ok, Sock} ->
-	    spawn(fun() -> recv_loop(Sock) end),
+	    spawn(fun() -> recv_loop(Sock, Dl_pid, Host, Peers_pid) end),
 	    Sock;
 	{error, _} ->
 	    error
     end.
 
-recv_loop(Socket) ->
+recv_loop(Socket, Dl_pid, Host, Peers_pid) ->
     case gen_tcp:recv(Socket, 0) of
-	{ok, Msg} ->
-	    io:format("~w~w~n", ["From handshaken", Msg]),
-	    recv_loop(Socket);
+	{ok, <<Pstrlen:8/integer, 
+	       Pstr:(19*8), 
+	       Reserved:64, 
+	       Info_hash:160,
+	       Peer_id:160>>} ->
+	    io:format("GOT HANDSHAKE BACK~n~n~n"),
+	    Pid_h = handshake_handler:start(Dl_pid),
+	    Pid_h ! {handshake, self(), Pstrlen, Pstr, Reserved, <<Info_hash:160>>, Peer_id},
+	    receive
+		{reply, Pid_h, ok} ->
+		    io:format("HANDSHAKE BACK FROM TRACKERPEER PROVED~n~n~n"),
+		    insert_valid_peer(Peers_pid, Peer_id, Socket, Host),
+		    message_handler:start(Dl_pid, Socket),
+		    io:format("WAITING FOR MESSAGE FROM TRACKERPEER~n~n~n");
+		{reply, Pid_h, drop_connection} ->
+		    gen_tcp:close(Socket)
+	    end;
+	{ok, Data} ->	
+	    io:format(binary_to_list(Data) ++ "~n");
 	{error, Reason} ->
-	    io:format("~w~n", [Reason])
+	    io:format("FYFAN REASON: ~w~n", [Reason])
     end.
+insert_valid_peer(Peers_pid, Peer_id, Sock, Host) ->    
+    Peers_pid ! {insert_peer, self(), Sock, Peer_id, Host},
+    receive
+	{reply, Reply} ->
+	    Reply
+    end.
+
+insert_valid_peer(Peers_pid, Peer_id, Sock) ->
+    insert_valid_peer(Peers_pid, Peer_id, Sock, unknown).
+    %% A = gen_tcp:recv(Socket, 0),
+    %% io:format("Received handshake back~n"),
+    %% message_handler:start(Dl_pid, Socket),
+    %% io:format("~n").
