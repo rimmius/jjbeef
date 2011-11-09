ea4730e713b9e07c180bb6276a15382a9f4b93d2
Bruce Yinhe <haoqing.yinhe@gmail.com>
2011-11-09 14:01:00 +0100

    port_listener.erl & message_handler.erl

--- port_listener.erl
+++ port_listener.erl
@@ -7,23 +7,23 @@
 %%% Created : 18 Oct 2011 by  <Bruce@THINKPAD>
 %%%-------------------------------------------------------------------
 -module(port_listener).
--export([listen/2, start/2]).
+-export([listen/3, start/3]).
 
 -define(TCP_OPTIONS, [binary, {packet, 0}, {active, false}, {reuseaddr, true}]).
 
-start(Port, Dl_pid) ->
-    spawn(?MODULE, listen, [Port, Dl_pid]).
+start(Port, Dl_pid, Parent) ->
+    spawn(?MODULE, listen, [Port, Dl_pid, Parent]).
 
-listen(Port, Dl_pid) ->
+listen(Port, Dl_pid, Parent) ->
     {ok, LSocket} = gen_tcp:listen(Port, ?TCP_OPTIONS),
-    accept(LSocket, Dl_pid).
+    accept(LSocket, Dl_pid, Parent).
 
-accept(LSocket, Dl_pid) ->
+accept(LSocket, Dl_pid, Parent) ->
     {ok, Socket} = gen_tcp:accept(LSocket),
-    spawn(fun() -> recv_loop(Socket, Dl_pid) end),
-    accept(LSocket, Dl_pid).
+    spawn(fun() -> recv(Socket, Dl_pid, Parent) end),
+    accept(LSocket, Dl_pid, Parent).
 
-recv_loop(Socket, Dl_pid) ->
+recv(Socket, Dl_pid, Parent) ->
     case gen_tcp:recv(Socket, 0) of
 	{ok, <<Pstrlen:8/integer, 
 	       Pstr:(19*8), 
@@ -44,24 +44,29 @@ recv_loop(Socket, Dl_pid) ->
 		    Msg = list_to_binary([<<19>>,<<"BitTorrent protocol">>, <<3,2,1,3,2,1,2,3>>, My_info_hash, list_to_binary(My_peer_id)]),
 		    ok = gen_tcp:send(Socket, Msg),
 		    io:format("Min handshake skickat TILLBAKA!~n"),
-		    Pid_m = message_handler:start(Dl_pid, Socket),
+		    
+		    %% ----------------------
+		    
+		    ok = peers:insert_valid_peer(Parent, Peer_id, Socket),
+		    
+		    message_handler:start(Dl_pid, Socket),
 
-		    io:format("Handshaken, I'm waiting for message~n"),
-		    receive
-			{reply, Pid_m, Reply} ->
-			    io:format("MSG hdler reply: ~w~n", [Reply])
-		    end,
+		    io:format("Handshaken, I'm waiting for message~n");
+		    %%  receive
+		    %%{reply, Pid_m, Reply} ->
+		    %%io:format("MSG hdler reply: ~w~n", [Reply])
+		    %%  end,
 		    
 		    %% -------------------------
-		    recv_loop(Socket, Dl_pid);
-		{reply, Pid_h, drop_connection} ->
+		    %% recv_loop(Socket, Dl_pid);
+		{reply, _Pid_h, drop_connection} ->
 		    gen_tcp:close(Socket)
 	    end;
 	   
 	{ok, Data} ->	
-	    io:format(binary_to_list(Data) ++ "~n"),
-	    recv_loop(Socket, Dl_pid);
-	{error, closed} ->
+	    io:format("~nWTF IS THISSSS???????? " ++ binary_to_list(Data) ++ "~n");
+	{error, Reason} ->
+	    io:format("FYfAN: ~w~n", [Reason]),
 	    ok
     end.
 
