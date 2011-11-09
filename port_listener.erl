9dd6b041fd5614a0f5bef40364404eecc61f883e
Bruce Yinhe <haoqing.yinhe@gmail.com>
2011-11-09 11:50:04 +0100

    message_handler.erl & port_listener.erl BIG changes

--- port_listener.erl
+++ port_listener.erl
@@ -32,23 +32,32 @@ recv_loop(Socket, Dl_pid) ->
 	       Peer_id:160>>} ->
 	    %% Handshake reader
 	    %% --------------------------	
-	    
+	    io:format("Got Handshake! Handshanke_handler started~n"),
 	    Pid_h = handshake_handler:start(Dl_pid),
-	    Pid_m = message_handler:start(),
 	    Pid_h ! {handshake, self(), Pstrlen, Pstr, Reserved, <<Info_hash:160>>, Peer_id},
+	    io:format("Waiting for Handshake handler reply~n"),
 	    receive 
 		{reply, Pid_h, ok} ->
-		    Pid_m ! {message, self(), Socket};	   
+		    io:format("Handshake proved!!!!~n"),
+		    My_peer_id = download_manager:get_my_id(Dl_pid),
+		    My_info_hash = download_manager:get_my_info_hash(Dl_pid),
+		    Msg = list_to_binary([<<19>>,<<"BitTorrent protocol">>, <<3,2,1,3,2,1,2,3>>, My_info_hash, list_to_binary(My_peer_id)]),
+		    ok = gen_tcp:send(Socket, Msg),
+		    io:format("Min handshake skickat TILLBAKA!~n"),
+		    Pid_m = message_handler:start(Dl_pid, Socket),
+
+		    io:format("Handshaken, I'm waiting for message~n"),
+		    receive
+			{reply, Pid_m, Reply} ->
+			    io:format("MSG hdler reply: ~w~n", [Reply])
+		    end,
+		    
+		    %% -------------------------
+		    recv_loop(Socket, Dl_pid);
 		{reply, Pid_h, drop_connection} ->
 		    gen_tcp:close(Socket)
-	    end,	
-	    io:format("Handshaken, I'm waiting for message~n"),
-	    receive
-		{reply, Pid_m, Reply} ->
-		    io:format("~w~n", [Reply])
-	    end,
-	    %% -------------------------
-	    recv_loop(Socket, Dl_pid);
+	    end;
+	   
 	{ok, Data} ->	
 	    io:format(binary_to_list(Data) ++ "~n"),
 	    recv_loop(Socket, Dl_pid);
