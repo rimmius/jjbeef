57ae1748627004d82a1fda988b3b3033431177c8
Jing Liu <kinajing@gmail.com>
2011-11-10 15:28:31 +0100

    fixed some bugs in temp_storage

--- temp_storage.erl
+++ temp_storage.erl
@@ -2,10 +2,11 @@
 %%%Creation date: 2011-11-08
 %%%
 -module(temp_storage).
--export([create_ets/1,insert/3,retrieve_all_data/0]).
+-export([create_ets/0,insert/3,retrieve_all_data/0]).
 
-create_ets(List)->
-    ets:new(downloadedPieces,[named_table,ordered_set]).
+%%Create a new ets table to store the downloaded pieces.
+create_ets()->
+    ets:new(downloadedPieces,[named_table]).
 
 insert(Index,Hash,Data)->
     ets:insert(downloadedPieces,{Index,{Hash,Data}}).
@@ -13,7 +14,7 @@ insert(Index,Hash,Data)->
 retrieve_all_data()->
     Length = ets:last(downloadedPieces),
     retrieve_all_data(1,Length).
-retrieve_all_data(N,Length)when N<=Length ->
+retrieve_all_data(N,Length)when N=<Length ->
     {_Index,{_Hash,Data}} = ets:lookup(downloadedPieces,N),
     [Data|retrieve_all_data(N+1,Length)];
 retrieve_all_data(_N,_Length) ->
