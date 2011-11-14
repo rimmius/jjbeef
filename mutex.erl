3d2b6a4ec1499c697f8339d4c0d48f29bb7de64e
eva-lisa <guskedev@student.gu.se>
2011-11-14 14:58:06 +0100

    functions to store peer info added

--- mutex.erl
+++ mutex.erl
@@ -1,53 +1,81 @@
-%%%Created by: Francesco Cesarini,Simon Thompson 
-%%%from the book Erlang Programming page 130 
-%%%Modified by: Eva-Lisa Kedborn, Jing Liu
-%%%Creation date: 2011-11-08
-%%%This module supervises the torrent parser module
+%%% Created by: Francesco Cesarini,Simon Thompson 
+%%% from the book Erlang Programming page 130 
+%%% Modified by: Eva-Lisa Kedborn, Jing Liu
+%%% Creation date: 2011-11-08
 
 -module(mutex).
--export([start/0, stop/0]).
--export([write/3, received/0]).
+-export([start/0, stop/1]).
+-export([write_file/4, write_new_peer/6, update_peer/5, received/1]).
 -export([init/0]).
 
 start() ->
-    register(mutex, spawn(?MODULE, init, [])).
+    spawn(?MODULE, init, []).
 
-stop() ->
-    mutex ! stop.
+stop(MutexPid) ->
+    MutexPid ! stop.
 
-write(Index,Hash,Data)->
-    mutex ! {write,Index,Hash,Data,self()},
-    receive {reply,Reply} -> 
+%% this will be used later for file storage
+write_file(MutexPid, Index, Hash, Data)->
+    MutexPid ! {write_file, Index, Hash, Data, self()},
+    receive {reply, Reply} -> 
 	    Reply
     end.
 
-received() ->
-    mutex ! {received, self()}, ok.
+%% send peer info to mutex for storage in db
+write_new_peer(MutexPid, Db, Ip, PeerId, Socket, Port) ->
+    MutexPid ! {write_new_peer, Db, Ip, PeerId, Socket, Port, self()},
+    receive {reply, Reply} -> 
+	    Reply
+    end.  
+
+%% send updated peer info to mutex for storage in db
+update_peer(MutexPid, PeerId, Db, Elem, Value) ->  
+    MutexPid ! {update_peer, PeerId, Db, Elem, Value, self()},
+    receive {reply, Reply} -> 
+	    Reply
+    end.
+
+%% once client received reply the mutex should be set to free
+received(MutexPid) ->
+    MutexPid ! {received, self()}, ok.
 
 init() ->
     free().
 
 free() ->
     receive
-	{write,Index,Hash,Data,Pid}->
-	    Has_inserted = temp_storage:insert(Index,Hash,Data),
-	    Pid!{reply,Has_inserted},	    
-	    busy(Pid);
+	{update_peer, PeerId, Db, Elem, Value, ClientPid} ->
+	    %% updated peer info in db. if successful atom true is returned
+	    Has_updated = temp_storage:update_peer(PeerId, Db, Elem, Value),
+	    ClientPid ! {reply, Has_updated},
+	    busy(ClientPid);
+	{write_new_peer, Db, Ip, PeerId, Socket, Port, ClientPid} ->
+	    %% store peer info in db. if successful atom true is returned
+	    Has_added = temp_storage:insert_new_peer(Db, Ip, PeerId, Socket, 
+						     Port),
+	    ClientPid ! {reply, Has_added},
+	    busy(ClientPid);
+	{write, Index, Hash, Data, ClientPid}->
+	    %% this will be used later for file storage
+	    Has_inserted = temp_storage:insert(Index, Hash, Data),
+	    ClientPid ! {reply, Has_inserted},	    
+	    busy(ClientPid);
 	stop ->
 	    terminate()
     end.
 
-busy(Pid) ->
+busy(ClientPid) ->
     receive
-	{received, Pid} ->
+	{received, ClientPid} ->
 	    free()
     end.
 
 terminate() ->
     receive
-	{request, Pid} ->
-	    exit(Pid, kill),
+	{_} ->
+	    exit(ClientPid, kill),
 	    terminate()
     after
 	0 -> ok
     end.
+
