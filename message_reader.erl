5ee1e90a1af67f76e5883a2ebe46ac21f4585e89
Bruce Yinhe <haoqing.yinhe@gmail.com>
2011-11-16 15:24:05 +0100

    message_reader.erl with adjustions to peer_storage.erl

--- message_reader.erl
+++ message_reader.erl
@@ -7,32 +7,37 @@ start(Parent, Peer_id) ->
 
 loop(Parent, Peer_id) ->
     receive
-	{bitfield, Bitfield} ->
-	    mutex:update_peer(Parent, Peer_id, bitfield, lol(Bitfield, 0)),
-	    mutex:received(Parent),
+	{bitfield, Bitfield, Bitfield_len} ->
+	    io:format("~nBitfiled: ~w~n", [lol(<<Bitfield:Bitfield_len>>, 0)]),
+ 	    peer_storage:update_peer(Parent, Peer_id, bitfield, lol(<<Bitfield:Bitfield_len>>, 0)),
+ 	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id);
 	{have, Piece_index} ->
-	    Old_bitfield = mutex:get_field(Parent, Peer_id, bitfield),
-	    mutex:received(Parent),
+	    Old_bitfield = peer_storage:read_field(Parent, Peer_id, bitfield),
+	    peer_storage:received(Parent),
 	    New_bitfield = lists:keyreplace(Piece_index, 2, Old_bitfield, {1, Piece_index}),
-	    mutex:update_peer(Parent, Peer_id, bitfield, New_bitfield),
-	    mutex:received(Parent),
+	    peer_storage:update_peer(Parent, Peer_id, bitfield, New_bitfield),
+	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id);
 	{choked, 1} ->
-	    mutex:update_peer(Parent, Peer_id, choke, 1),
-	    mutex:received(Parent),
+	    peer_storage:update_peer(Parent, Peer_id, choke, 1),
+	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id);
 	{choked, 0} ->
-	    mutex:update_peer(Parent, Peer_id, choke, 0),
-	    mutex:received(Parent),
+	    peer_storage:update_peer(Parent, Peer_id, choke, 0),
+	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id);
 	{interested, 1} ->
-	    mutex:update_peer(Parent, Peer_id, interested, 1),
-	    mutex:received(Parent),
+	    peer_storage:update_peer(Parent, Peer_id, interested, 1),
+	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id);
 	{interested, 0} ->
-	    mutex:update_peer(Parent, Peer_id, interested, 0),
-	    mutex:received(Parent),
+	    peer_storage:update_peer(Parent, Peer_id, interested, 0),
+	    peer_storage:received(Parent),
+	    loop(Parent, Peer_id);
+	{port, Listen_port} ->
+	    peer_storage:update_peer(Parent, Peer_id, port, Listen_port),
+	    peer_storage:received(Parent),
 	    loop(Parent, Peer_id)
     end.
 
