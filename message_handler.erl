2352ee6ada7999af4da42493988495c144011339
Bruce Yinhe <haoqing.yinhe@gmail.com>
2011-11-16 11:39:16 +0100

    message_handler.erl and message_reader.erl fit with new mutex.erl

--- message_handler.erl
+++ message_handler.erl
@@ -9,22 +9,27 @@
 -module(message_handler).
 -export([start/4, recv_loop/4]).
 
-start(Parent, Table_id, Socket, Peer_id) ->
-    spawn(?MODULE, recv_loop, [Parent, Table_id, Socket, Peer_id]).
+start(Parent, Socket, Peer_id) ->
+    spawn(?MODULE, init, [Parent, Socket, Peer_id]).
 
-recv_loop(Parent, Table_id, Socket, Peer_id) ->    
+init(Parent, Socket, Peer_id) ->
+    Pid_message_reader = message_reader:start(Parent, Peer_id),
+    recv_loop(Parent, Socket, Peer_id, Pid_message_reader).
+
+recv_loop(Parent, Socket, Peer_id, Pid_message_reader) ->    
     case gen_tcp:recv(Socket, 4) of
 	%% case {ok, Socket} of %% for testing
 	{ok, <<0,0,0,0>>} ->
 	    %% keep-alive
 	    io:format("~n~n**********keep-alive len=0~n"),
 	    Parent ! {reply, self(), "Replied"},
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, <<0,0,0,1>>} ->
 	    case gen_tcp:recv(Socket, 1) of
 		{ok, <<0>>} ->
 		    %%choke
-		    io:format("~n*****Choke len=1, id=0~n");
+		    io:format("~n*****Choke len=1, id=0~n"),
+		    Pid_message_reader ! {choke};
 		{ok, <<1>>} ->
 		    %% unchoke
 		    io:format("~n*****Unchoke len=1, id=1~n");
@@ -35,14 +40,14 @@ recv_loop(Parent, Table_id, Socket, Peer_id) ->
 		    %% uninterested
 		    io:format("~n*****Uninterested len=1, id=3~n")
 	    end,
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, <<0,0,0,5>>} ->
 	    case gen_tcp:recv(Socket, 5) of
 		{ok, <<4, Piece_index:32>>} ->
 		    %%have
 		    io:format("~n*****Have len=5, id=4, piece_index=~w~n", [Piece_index])
 	    end,
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, <<0,0,0,13>>} ->
 	    case gen_tcp:recv(Socket, 13) of
 		{ok, <<6, Index:32, Begin:32, Length:32>>} ->
@@ -52,14 +57,14 @@ recv_loop(Parent, Table_id, Socket, Peer_id) ->
 		    %%cancel
 		    io:format("~n*****Cancel len=13, id=8, index=~w, begin=~w, length=~w~n", [Index, Begin, Length])
 	    end,
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, <<0,0,0,3>>} ->
 	    case gen_tcp:recv(Socket, 3) of
 		{ok, <<9, Listen_port:16>>} ->
 		    %%port
 		    io:format("~n*****Port len=3, id=9, listen_port=~w~n", [Listen_port])
 	    end,
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, <<Len:32/integer-big>>} ->
 	    Bitfield_len = Len*8-8,
 	    Block_len = Len*8-72,
@@ -67,16 +72,15 @@ recv_loop(Parent, Table_id, Socket, Peer_id) ->
 		{ok, <<5, Bitfield:Bitfield_len>>} ->
 		    %%bitfield
 		    io:format("~n*****bitfield len=1+~w, id=5, bitfield=~w~n", [Len-1, Bitfield]),
-		    Pid_message_reader = message_reader:start(Parent, Peer_id),
-		    Pid_message_reader ! {bitfield, Bitfield};
+		   Pid_message_reader ! {bitfield, Bitfield};
 		{ok, <<7, Index:32, Begin:32, Block:Block_len>>} ->
 		    %%piece
 		    io:format("~n*****piece len=9+~w, id=7, index=~w, begin=~w, block=~w~n", [Len-9, Index, Begin, Block])
 	    end,
-	    recv_loop(Parent, Table_id, Socket, Peer_id);
+	    recv_loop(Parent, Socket, Peer_id);
 	{ok, _Data} ->
 	    io:format("~nother messages, cannot read~n"),
-	    recv_loop(Parent, Table_id, Socket, Peer_id); %% not sure
+	    recv_loop(Parent, Socket, Peer_id); %% not sure
 	{error, Reason} ->
 	    io:format("~nmessage receiving error: ~w~n", [Reason]);
 	_ ->
--- message_reader.erl
+++ message_reader.erl
@@ -2,18 +2,31 @@
 -export([start/3]).
 -export([loop/3]).
 
-start(Parent, Table_id, Peer_id) ->
-    spawn(?MODULE, loop, [Parent, Table_id, Peer_id]).
+start(Parent, Peer_id) ->
+    spawn(?MODULE, loop, [Parent, Peer_id]).
 
-loop(Parent, Table_id, Peer_id) ->
+loop(Parent, Peer_id) ->
     receive
 	{bitfield, Bitfield} ->
-	    mutex:update_peer(Parent, Peer_id, Table_id, bitfield, lol(Bitfield)),
-	    loop(Parent, Table_id, Peer_id)
+	    mutex:update_peer(Parent, Peer_id, bitfield, lol(Bitfield, 0)),
+	    loop(Parent, Peer_id);
+	{have, Piece_index} ->
+	    Old_bitfield = mutex:get_field(Parent, Peer_id, bitfield),
+	    New_bitfield = lists:keyreplace(Piece_index, 2, Old_bitfield, {1, Piece_index}),
+	    mutex:update_peer(Parent, Peer_id, bitfield, New_bitfield),
+	    loop(Parent, Peer_id);
+	{choke} ->
+	    mutex:update_peer(Parent, Peer_id, choke, 1),
+	    loop(Parent, Peer_id);
+	{unchoke} ->
+	    mutex:update_peer(Parent, Peer_id, choke, 0),
+	    loop(Parent, Peer_id)
+		
+		
     end.
 
-lol(<<P:1>>) ->
-    [P];
-lol(<<P:1, Rest/bits>>) ->
-    [P | lol(Rest)].
+lol(<<P:1>>, Index) ->
+    [{P, Index}];
+lol(<<P:1, Rest/bits>>, Index) ->
+    [{P, Index} | lol(Rest, Index+1)].
      
