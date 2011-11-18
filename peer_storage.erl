851f8ef9a058660127d930e4a8de731282d37949
Jing Liu <kinajing@gmail.com>
2011-11-18 12:07:58 +0100

    new attribute Request added and some io format added

--- peer_storage.erl
+++ peer_storage.erl
@@ -5,11 +5,11 @@
 
 -module(peer_storage).
 -export([start/0,init/0,stop/1]).
--export([received/1, insert_new_peer/5, update_peer/4,read_field/3]).
+-export([received/1, insert_new_peer/6, update_peer/4,read_field/3]).
 
 -record(peer, {peerid = undefined, interested = 0, choke = 1, 
 	       ip = undefined, socket = undefined,
-	       port = undefined}).
+	       port = undefined, request = undefined}).
 
 start() ->
     spawn(?MODULE, init, []).
@@ -26,8 +26,8 @@ received(Pid) ->
     Pid ! {received, self()}, ok.
 
 %% send peer info to mutex for storage in db
-insert_new_peer(Pid, Ip, PeerId, Socket, Port) ->
-    Pid ! {insert_new_peer, Ip, PeerId, Socket, Port, self()},
+insert_new_peer(Pid, Ip, PeerId, Socket, Port,Request) ->
+    Pid ! {insert_new_peer, Ip, PeerId, Socket, Port, Request,self()},
     receive {reply, Reply} -> 
 	    Reply
     end.  
@@ -50,14 +50,26 @@ free(Tid) ->
     receive
 	{update_peer, PeerId, Field, Value, ClientPid} ->
 	    %% updated peer info in db. if successful atom true is returned
-	    ClientPid!{reply,update_peer_local(Tid,PeerId,Field,Value)},
+	    Reply = update_peer_local(Tid,PeerId,Field,Value),
+	    case Reply of 
+		true->io:format("we have updated ~w, ~w, ~w, ~w~n",
+				[Tid,PeerId,Field,Value])
+	    end,
+	    ClientPid!{reply,Reply},
 	    busy(ClientPid,Tid);
-	{insert_new_peer, Ip, PeerId, Socket, Port, ClientPid} ->
+	{insert_new_peer, Ip, PeerId, Socket, Port, Request, ClientPid} ->
 	    %% store peer info in db. if successful atom true is returned
-	    ClientPid!{reply,insert_new_peer_local(Tid,Ip,PeerId, Socket, Port)},
+	    Reply = insert_new_peer_local(Tid,Ip,PeerId, Socket, Port,Request),
+	    case Reply of 
+		true->io:format("we have inserted ~w, ~w, ~w, ~w, ~w, ~w~n",
+				[Tid,Ip,PeerId,Socket,Port,Request])
+	    end,
+	    ClientPid!{reply,Reply},
 	    busy(ClientPid,Tid);
 	{read_field,PeerId,Field,ClientPid}->
-	    ClientPid!{reply,read_field_local(Tid,PeerId,Field)},
+	    Reply = read_field_local(Tid,PeerId,Field),
+	    io:format("we have read ~w, ~w, ~w~n",[Tid,PeerId,Field]),
+	    ClientPid!{reply,Reply},
 	    free(Tid);
 	stop -> ok
     end.
@@ -69,9 +81,9 @@ busy(ClientPid,Tid) ->
     end.
 
 %% insert peer for the first time.
-insert_new_peer_local(Tid, Ip, PeerId, Socket, Port) -> 
+insert_new_peer_local(Tid, Ip, PeerId, Socket, Port,Request) -> 
     ets:insert(Tid, #peer{peerid = PeerId, interested = 0, choke = 1,
-			  ip = Ip, socket = Socket, port = Port}).
+			  ip = Ip, socket = Socket, port = Port,request = Request}).
 
 %% update certain peer fields
 update_peer_local(Tid,PeerId, Field, Value) ->
@@ -80,7 +92,8 @@ update_peer_local(Tid,PeerId, Field, Value) ->
 					   interested = Value});
 	choke -> ets:insert(Tid, #peer{peerid = PeerId, choke = Value});
 	socket -> ets:insert(Tid, #peer{peerid = PeerId, socket = Value});
-	port -> ets:insert(Tid, #peer{peerid = PeerId, port = Value})
+	port -> ets:insert(Tid, #peer{peerid = PeerId, port = Value});
+	request -> ets: insert(Tid, #peer{peerid = PeerId,request = Value})
     end.
 
 %% read certain peer fields and return their value
@@ -90,6 +103,7 @@ read_field_local(Tid, PeerId, Field) ->
 	interested -> Peer#peer.interested;
 	choke -> Peer#peer.choke;
 	socket -> Peer#peer.socket;
-	port -> Peer#peer.port
+	port -> Peer#peer.port;
+	request -> Peer#peer.request
     end.	    
 
