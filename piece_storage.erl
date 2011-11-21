80bc6c88c6411b8538107f31dbb774fb71fed027
Jing Liu <kinajing@gmail.com>
2011-11-22 00:26:52 +0100

    get_piecehash and update_bitfield added, with comment

--- piece_storage.erl
+++ piece_storage.erl
@@ -33,6 +33,12 @@ loop(Tid)->
 		read_piece ->
 		    Index = Args,
 		    Reply = read_piece(Tid, Index);
+		update_bitfield ->
+		    [PeerId,PieceIndex] = Args,
+		    Reply = update_bitfield(Tid,PeerId,PieceIndex);
+		get_piece_hash ->
+		    [Index]=Args,
+		    Reply = get_piece_hash(Tid,Index);
 		putback ->
 		    Piece = Args,
 		    Reply = putback(Tid, Piece)
@@ -42,20 +48,35 @@ loop(Tid)->
 	stop -> ok
     end.
 
+%%insert a new bitfield into the table
 insert_bitfield(Tid, PeerId, [H|T])->
     Has = [X || {1, X} <- [H|T]],
     insert_to_table(Has, PeerId, Tid).
 
+%%inner function of insert_birfield
 insert_to_table([Has|T], PeerId, Tid)->
-    [Index, {Hash, Peers}] = ets:lookup(Tid, Has),
+    [{Index, {Hash, Peers}}] = ets:lookup(Tid, Has),
      ets:insert(Tid, {Index, {Hash, [PeerId|Peers]}}),
      insert_to_table(T, PeerId, Tid);
 insert_to_table([], _PeerId, _Tid) ->
      has_inserted.
 
+%%update piece storage when a have message is received
+update_bitfield(Tid,PeerId,PieceIndex)->
+    [{PieceIndex,{Hash,Peers}}]=ets:lookup(Tid,PieceIndex),
+    ets:insert(Tid,{PieceIndex,{Hash,[PeerId|Peers]}}),
+    has_updated.
+
+%%read the list of peers that has a certain piece by 
+%%providing the piece index . 
 read_piece(Tid, Index)->
     [Content] = ets:lookup(Tid, Index),
     Content.
+
+%%get the piecehash of a piece by providing the piece index
+get_piece_hash(Tid,Index)->
+    [{Index,{Piecehash,_Peers}}] = ets:lookup(Tid,Index),
+    Piecehash.
 
 %% insert the piece returned from downloading_storage
 putback(Tid, Piece)->
