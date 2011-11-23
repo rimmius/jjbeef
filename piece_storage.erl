f5cce6ea12a50c4709d78f51966babb40b16ea20
eva-lisa <guskedev@student.gu.se>
2011-11-23 11:29:09 +0100

    test code added and ets table cahnged to named

--- piece_storage.erl
+++ piece_storage.erl
@@ -2,83 +2,114 @@
 %%% Creation date: 2011-11-16
 
 -module(piece_storage).
--export([start/1, init/1, stop/1, loop/1]).
+-export([start/1, init/1, initiate_table/1]).
 
-start(List)->
+-include_lib("eunit/include/eunit.hrl").
+
+start(List) ->
     spawn(?MODULE, init, [List]).
 
-init(List)->
-    Tid = ets:new(db,[ordered_set]),
-    initiate_table(List, Tid),
-    loop(Tid).
+init(List) ->
+    initiate_table(List),
+    loop(piece_table).
 
-stop(Pid)->
-    Pid ! stop.
+initiate_table(List) ->
+    ets:new(piece_table,[named_table, ordered_set]),
+    initiate_table(piece_table, List, 0).
+initiate_table(piece_table, [H|T], Index) ->
+    ets:insert(piece_table, {Index, {H, []}}),
+    initiate_table(piece_table, T, Index + 1);
+initiate_table(piece_table, [], _Index) ->
+    piece_table.
 
-initiate_table(List,Tid)->
-    initiate_table(List, Tid, 0).
-initiate_table([H|T], Tid, Index)->
-    ets:insert(Tid, {Index, {H, []}}),
-    initiate_table(T, Tid, Index + 1);
-initiate_table([], _Tid, _Index)->
-    done.
-
-loop(Tid)->
+loop(piece_table)->
     receive
 	{request, Function, Args, From} ->
 	    case Function of
 		insert_bitfield ->
 		    [PeerId, [H|T]] = Args,
-		    Reply = insert_bitfield(Tid, PeerId, [H|T]);
+		    Reply = insert_bitfield(piece_table, PeerId, [H|T]);
 		read_piece ->
 		    Index = Args,
-		    Reply = read_piece(Tid, Index);
+		    Reply = read_piece(piece_table, Index);
 		update_bitfield ->
-		    [PeerId,PieceIndex] = Args,
-		    Reply = update_bitfield(Tid,PeerId,PieceIndex);
+		    [PeerId, PieceIndex] = Args,
+		    Reply = update_bitfield(piece_table, PeerId, PieceIndex);
 		get_piece_hash ->
-		    [Index]=Args,
-		    Reply = get_piece_hash(Tid,Index);
+		    [Index] = Args,
+		    Reply = get_piece_hash(piece_table, Index);
 		putback ->
 		    Piece = Args,
-		    Reply = putback(Tid, Piece)
+		    Reply = putback(piece_table, Piece)
 	    end,
-	    From!{reply, Reply},
-	    loop(Tid);
+	    From ! {reply, Reply},
+	    loop(piece_table);
 	stop -> ok
     end.
 
-%%insert a new bitfield into the table
-insert_bitfield(Tid, PeerId, [H|T])->
+%% insert a new bitfield into the table
+insert_bitfield(piece_table, PeerId, [H|T]) ->
     Has = [X || {1, X} <- [H|T]],
-    insert_to_table(Has, PeerId, Tid).
+    insert_to_table(Has, PeerId, piece_table).
 
-%%inner function of insert_birfield
-insert_to_table([Has|T], PeerId, Tid)->
-    [{Index, {Hash, Peers}}] = ets:lookup(Tid, Has),
-     ets:insert(Tid, {Index, {Hash, [PeerId|Peers]}}),
-     insert_to_table(T, PeerId, Tid);
-insert_to_table([], _PeerId, _Tid) ->
+%% inner function of insert_bitfield
+insert_to_table([Has|T], PeerId, piece_table) ->
+    [{Index, {Hash, Peers}}] = ets:lookup(piece_table, Has),
+     ets:insert(piece_table, {Index, {Hash, [PeerId|Peers]}}),
+     insert_to_table(piece_table, T, PeerId);
+insert_to_table(piece_table, [], _PeerId) ->
      has_inserted.
 
-%%update piece storage when a have message is received
-update_bitfield(Tid,PeerId,PieceIndex)->
-    [{PieceIndex,{Hash,Peers}}]=ets:lookup(Tid,PieceIndex),
-    ets:insert(Tid,{PieceIndex,{Hash,[PeerId|Peers]}}),
-    has_updated.
+%% update piece storage when a have message is received
+update_bitfield(piece_table, PeerId, PieceIndex) ->
+    [{PieceIndex, {Hash, Peers}}] = ets:lookup(piece_table, PieceIndex),
+    Is_updated = ets:insert(piece_table, {PieceIndex, {Hash, [PeerId|Peers]}}),
+    Is_updated.
 
-%%read the list of peers that has a certain piece by 
-%%providing the piece index . 
-read_piece(Tid, Index)->
-    [Content] = ets:lookup(Tid, Index),
+%% read the list of peers that has a certain piece by 
+%% providing the piece index. 
+read_piece(piece_table, Index) ->
+    [Content] = ets:lookup(piece_table, Index),
     Content.
 
-%%get the piecehash of a piece by providing the piece index
-get_piece_hash(Tid,Index)->
-    [{Index,{Piecehash,_Peers}}] = ets:lookup(Tid,Index),
+%% get the piecehash of a piece by providing the piece index
+get_piece_hash(piece_table, Index) ->
+    [{Index, {Piecehash, _Peers}}] = ets:lookup(piece_table, Index),
     Piecehash.
 
 %% insert the piece returned from downloading_storage
-putback(Tid, Piece)->
+putback(piece_table, Piece)->
     {PieceIndex, {Piecehash, AllPeersList}} = Piece,
-    ets:insert(Tid, {PieceIndex, {Piecehash, AllPeersList}}).
+    ets:insert(piece_table, {PieceIndex, {Piecehash, AllPeersList}}).
+
+
+
+%% TEST CASES %%
+
+%% Id:                 1 
+%% Title:              Initiate piece table
+%% Purpose:            Ensure correct creation of table and first insertion of 
+%%                     pieces and their hashes
+%% Prerequisites:      None
+%% Expected result:    The table has been created and pieces correctly inserted
+%% Pass/Fail criteria: When run response is "All 2 tests passed"/When run 
+%%                     response is error
+
+initiate_table_test_() ->
+    {spawn,
+     {setup,
+      fun() ->
+	      initiate_table([a,b,c])
+      end,
+      fun(_) ->
+	      ets:delete(piece_table) 
+      end,
+      [?_assertMatch([], ets:lookup(test, 3)),
+       ?_assertMatch([{0, {a, []}}], ets:lookup(test, 0))]
+     }
+    }.    
+
+	      
+	      
+	      
+
