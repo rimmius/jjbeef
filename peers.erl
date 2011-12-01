5c9bcbb85453b4915d850c44800119665214806a
gUstehn <fredrikgustafsson84@gmail.com>
2011-12-01 11:13:04 +0100

    chnaged some shiet

--- peers.erl
+++ peers.erl
@@ -48,7 +48,7 @@ loop(Peer_storage_pid, File_storage_pid, Piece_storage_pid, Dl_storage_pid) ->
 	    mutex:request(Peer_storage_pid, insert_new_peer, [Host,Peer_id, 
 					 Sock, Port, undefined]),
 	    mutex:received(Peer_storage_pid),
-	    piece_requester:start_link(Peer_storage_pid, Piece_storage_pid, File_storage_pid, Sock, Peer_id),
+	    piece_requester:start_link(Peer_storage_pid, Piece_storage_pid, File_storage_pid, Dl_storage_pid, Sock, Peer_id),
 	    From ! {reply, ok},
 	    io:format("~n~nIN THE LOOP SENT MESS BACK"),
 	    loop(Peer_storage_pid, File_storage_pid, Piece_storage_pid, Dl_storage_pid);
