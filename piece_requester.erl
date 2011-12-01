d66a5ef389ee053dc6343f353b623226bccb0711
Bruce Yinhe <haoqing.yinhe@gmail.com>
2011-12-01 11:37:49 +0100

    piece_requester lol

--- piece_requester.erl
+++ piece_requester.erl
@@ -147,10 +147,16 @@ am_choked_interested(am_unchoked, State) ->
 am_unchoked_interested(am_choked, State) ->
     {next_state, am_choked_interested, State};
 am_unchoked_interested({piece_complete, Index}, State) ->
-	Reply = mutex:request(State#state.piece_storage, get_rarest_index, State#state.peer_id),
-	mutex:received(State#state.piece_storage),
+    Reply = mutex:request(State#state.piece_storage, get_rarest_index, State#state.peer_id),
+    mutex:received(State#state.piece_storage),
     case Reply of
-	{ok, Index} -> 
+	{ok, Index, Data} -> 
+	    %% write the piece into dl_sto and remove it from piece_sto
+	    mutex:request(State#state.download_storage, write_piece, [Index, Data, self()]),
+	    mutex:received(State#state.download_storage),
+	    mutex:request(State#state.piece_storage, delete_piece, [Index]),
+	    mutex:received(State#state.piece_storage),
+
 	    {Begin, Length} = mutex:request(State#state.file_storage, what_chunk, [Index]),
 	    mutex:received(State#state.file_storage),
 	    io:format("Rdy to request a New chunk of a new piece: index=~w, begin=~w, length=~w~n", [Index, Begin, Length]), 
