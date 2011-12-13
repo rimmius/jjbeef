c5343e9f65e37c0c2ca6a46e8c44872ab7ca8529
Jing Liu <kinajing@gmail.com>
2011-12-13 13:51:26 +0100

    comment added in downloading_storage

--- downloading_storage.erl
+++ downloading_storage.erl
@@ -8,11 +8,23 @@
 start() ->
     spawn(?MODULE, init, []).
 
-%% get the pid of piece_storage to be able to put the piece back
+%%--------------------------------------------------------------------
+%% Function: init/0
+%% Purpose: Create an ets table to store the pieces being downloaded.
+%% Args: none
+%% Returns: TableID of the downloading table.
+%%--------------------------------------------------------------------
 init() ->
     Tid = ets:new(downloading_table, []),
     loop(Tid).
 
+%%--------------------------------------------------------------------
+%% Function:loop/1
+%% Purpose: receive requests from downloading mutex about what functions to
+%%          execute
+%% Args:  TableID of downloading table
+%% Returns: the requested information
+%%--------------------------------------------------------------------
 loop(Tid) ->
     receive
 	{request, Function, Args, From} ->
@@ -38,11 +50,26 @@ loop(Tid) ->
 	stop -> ok	
     end.
 
-%% insert new piece that has chosen to be downloaded
+%%--------------------------------------------------------------------
+%% Function:write/4
+%% Purpose: insert new piece that has been  chosen to download
+%% Args:  TableID of downloading table, piece index, the piece, processID
+%%        of the peer.
+%%--------------------------------------------------------------------
 write(Tid, PieceIndex, Tuple, Pid) ->
     {PieceIndex, {Hash, Peers}} = Tuple,
     ets:insert(Tid, {{Pid, PieceIndex}, {PieceIndex, {Hash, Peers}}}).
 
+%%--------------------------------------------------------------------
+%% Function: put_back/3
+%% Purpose: After a piece is downloaded, if its hash is corruped,this 
+%%          function takes its piece index and peerPID,tries to find it
+%%          in the downloading table, and returns the full piece info 
+%%          to be put back to the piece table.
+%% Args:  TableID of downloading table, ProcessId, piece index
+%% Returns: either [] if no record is found, or {Index,{Hash,Peers}}which
+%%          is the piece
+%%--------------------------------------------------------------------
 put_back(Tid, Pid, Index) ->
     case ets:lookup(Tid, {Pid, Index}) of
 	[] ->
@@ -51,6 +78,15 @@ put_back(Tid, Pid, Index) ->
 	    {Index, {Hash, Peers}}
     end.
 
+%%--------------------------------------------------------------------
+%% Function: put_back_withou_only_pid/2
+%% Purpose: If a peer disconnects before we have received the entire
+%%          piece,this function will try to find a list of pieces downloaded
+%%          by this peer by its Pid. This list later will be put back to 
+%%          the piece table so they are available to other peers.
+%% Args: TableId of downloading table,Pid of the disconnected peer
+%% Returns: either an empty list if no record is found or a list of pieces
+%%--------------------------------------------------------------------
 put_back_with_only_pid(Tid, FilePid) ->
     First_key = ets:first(Tid),
     put_back_new(Tid, FilePid, First_key, []).
@@ -81,6 +117,14 @@ put_back_new(Tid, FilePid, Key, List) ->
 	   end
    end.
 
+%%--------------------------------------------------------------------
+%% Function: compare_hash/4
+%% Purpose: When a full piece has been downloaded,this method would verify
+%%          its hash value.
+%% Args: TableId of downloading table,ProcessId of the peer,piece index,
+%%       piece hash.
+%% Returns: true if the hash is good and false is the hash is corrupted
+%%--------------------------------------------------------------------
 compare_hash(Tid, Pid, FileIndex, FileHash) ->
     case ets:lookup(Tid, {Pid, FileIndex}) of
 	[]->
