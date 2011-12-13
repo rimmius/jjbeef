62e8494d823745fb169368a256ce8dde8e420130
eva-lisa <guskedev@student.gu.se>
2011-12-13 11:05:41 +0100

    loop data changed

--- downloading_storage.erl
+++ downloading_storage.erl
@@ -10,7 +10,7 @@ start() ->
 
 %% get the pid of piece_storage to be able to put the piece back
 init() ->
-    Tid = ets:new(db, []),
+    Tid = ets:new(downloading_table, []),
     loop(Tid).
 
 loop(Tid) ->
@@ -18,77 +18,77 @@ loop(Tid) ->
 	{request, Function, Args, From} ->
 	    case Function of
 		write_piece ->
-		    [PieceIndex, Tuple,Pid]
-			= Args,
-		    From ! {reply, write(Tid, PieceIndex, Tuple,Pid)};
+		    [PieceIndex, Tuple, Pid] = Args,
+		    From ! {reply, write(Tid, PieceIndex, Tuple, Pid)};
 		put_back ->
 		    [Pid,Index] = Args,
-		    From ! {reply, put_back(Tid, Pid,Index)};
+		    From ! {reply, put_back(Tid, Pid, Index)};
 		put_back_with_only_pid ->
 		    [Pid] = Args,
-		    From ! {reply,put_back_with_only_pid(Tid,Pid)};
+		    From ! {reply, put_back_with_only_pid(Tid, Pid)};
 		compare_hash ->
-		    [Pid,FileIndex,FileHash]= Args,
-		    From!{reply,compare_hash(Tid,Pid,FileIndex,FileHash)}
-
+		    [Pid, FileIndex, FileHash] = Args,
+		    From!{reply,compare_hash(Tid, Pid, FileIndex, FileHash)}
 	    end,
+	    loop(Tid);
+	{lookup, Data, From} -> 
+	    Result = ets:lookup(Tid, Data),
+	    From ! {reply, Result},
 	    loop(Tid);
 	stop -> ok	
     end.
 
 %% insert new piece that has chosen to be downloaded
-write(Tid, PieceIndex,Tuple,Pid) ->
-    {PieceIndex,{Hash,Peers}} = Tuple,
-    ets:insert(Tid, {{Pid,PieceIndex}, {PieceIndex,{Hash,Peers}}}).
+write(Tid, PieceIndex, Tuple, Pid) ->
+    {PieceIndex, {Hash, Peers}} = Tuple,
+    ets:insert(Tid, {{Pid, PieceIndex}, {PieceIndex, {Hash, Peers}}}).
 
-
-
-put_back(Tid, Pid,Index)->
-    case ets:lookup(Tid, {Pid,Index}) of
+put_back(Tid, Pid, Index) ->
+    case ets:lookup(Tid, {Pid, Index}) of
 	[] ->
 	    [];
-	[{{Pid,Index}, {Index, {Hash, Peers}}}] ->
+	[{{Pid, Index}, {Index, {Hash, Peers}}}] ->
 	    {Index, {Hash, Peers}}
     end.
 
-
-put_back_with_only_pid(Tid,FilePid)->
+put_back_with_only_pid(Tid, FilePid) ->
     First_key = ets:first(Tid),
-    put_back_new(Tid,FilePid,First_key,[]).
-put_back_new(Tid,FilePid,Key,List)->
-   case ets:lookup(Tid,Key) of
+    put_back_new(Tid, FilePid, First_key, []).
+put_back_new(Tid, FilePid, Key, List) ->
+   case ets:lookup(Tid, Key) of
        [] ->
 	   [];
-       [{{Pid,Index},{Index,{Hash,Peers}}}]  ->
+       [{{Pid, Index}, {Index, {Hash, Peers}}}] ->
 	   case Pid =:= FilePid of
 	       true->
-		   Next_key = ets:next(Tid,Key),
+		   Next_key = ets:next(Tid, Key),
 		   case Next_key of
 		       '$end_of_table'->
 			   [{Index, {Hash, Peers}}|List];
 		       _found ->
-			   put_back_new(Tid,FilePid,Next_key,[{Index,{Hash,Peers}}|List])
+			   put_back_new(Tid, FilePid, Next_key,
+					[{Index, {Hash, Peers}}|List])
 		   end;
 	       false ->
-		   Next_key = ets:next(Tid,Key),
+		   Next_key = ets:next(Tid, Key),
 		   case Next_key of
 		       '$end_of_table' ->
 			   List;
 		       _found ->
-			   put_back_new(Tid,FilePid,Next_key,List)
+			   put_back_new(Tid, FilePid, Next_key, List)
 			       
 		   end
-     end
-end.
+	   end
+   end.
 
-compare_hash(Tid,Pid,FileIndex,FileHash)->
-    case ets:lookup(Tid, {Pid, FileIndex})of
+compare_hash(Tid, Pid, FileIndex, FileHash) ->
+    case ets:lookup(Tid, {Pid, FileIndex}) of
 	[]->
 	    cannot_find_this_Pid;
-	[{{Pid,Index},{Index,{Hash,_Peers}}}] ->
-	    case Index==FileIndex of
+	[{{Pid, Index}, {Index, {Hash, _Peers}}}] ->
+	    case Index == FileIndex of
 		true->
-		    Hash=:=FileHash;
+		    Hash =:= FileHash;
 		false ->
 		    index_dont_match
 	    end
