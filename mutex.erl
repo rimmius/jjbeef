58e4c3f213d5eb5e41764565def7a7f0ad50117d
Jing Liu <kinajing@gmail.com>
2011-12-15 17:56:35 +0100

    heading added for mutex

--- mutex.erl
+++ mutex.erl
@@ -1,18 +1,47 @@
-%%% Created by: Francesco Cesarini, Simon Thompson 
-%%% from the book Erlang Programming page 130 
-%%% Modified by: Eva-Lisa Kedborn, Jing Liu
+%%%---------------------------------------------------------------------
+%%% Created by: Francesco Cesarini, Simon Thompson
+%%% from the book Erlang Programming page 130
+%%% Modified by: Eva-Lisa Kedborn,Jing Liu
 %%% Creation date: 2011-11-08
-%%% This module isolates process access to db storage to ensure data safety
+%%%--------------------------------------------------------------------- 
+%%% Description module mutex
+%%%--------------------------------------------------------------------- 
+%%% The mutex is a finite state machine that has two states:free and busy.
+%%% If the client wants to have access and change the content in the storage
+%%% tables, it must go through the mutex. It prevents two processes modifying
+%%% the table content at the same time.Isolating the process access to db
+%%% storages ensures data safety.
+%%%--------------------------------------------------------------------- 
+%%% Exports 
+%%%--------------------------------------------------------------------- 
+%%% start(Module,Args)
+%%%   spawns a new process for one of the storages and returns its PID
+%%%--------------------------------------------------------------------- 
+%%% stop()
+%%%   stops the mutex process 
+%%%--------------------------------------------------------------------- 
+%%% init(Module,Args)
+%%%   passes the PID of the spawned storage process into loop
+%%%---------------------------------------------------------------------  
+%%% received(MutexPid)
+%%%   After the client receives replies for what it has requested,this
+%%%   function is called to set the state of the mutex from busy to free
+%%%---------------------------------------------------------------------
+%%% request(MutexPid,Function,Args)
+%%%   Client uses this function to send requests to the database.MutexPid
+%%%   shows which storage the client wants to access. Function and Args are
+%%%   the specific requests. 
+%%%---------------------------------------------------------------------  
 
 -module(mutex).
 -export([start/2, stop/1]).
 -export([init/2, received/1, request/3]).
-%%changed this to two-arguments, if the start function takes arguments
+
 start(Module, Args) ->
     spawn(?MODULE, init, [Module, Args]).
 
 init(Module, Args) ->
-    StoragePid = apply(Module, start, Args), %%changed this to apply to make it easier to apply the arguments of the start function.
+    StoragePid = apply(Module, start, Args),
     link(StoragePid),
     free(StoragePid).
 
@@ -30,6 +59,14 @@ request(MutexPid, Function, Args) ->
 	    Reply 
     end.
 
+%%--------------------------------------------------------------------
+%% Function:free/1
+%% Purpose: One of the states.It receives requests from the client,
+%%          passes the requests to the storage process, and receives reply.
+%% Args: processID of the storage process
+%% Returns: what is being requested
+%%--------------------------------------------------------------------
+
 free(StoragePid) ->
     receive
 	{Function, Args, ClientPid} ->
@@ -42,6 +79,12 @@ free(StoragePid) ->
 	stop -> terminate(StoragePid)
     end.
 
+%%--------------------------------------------------------------------
+%% Function:busy/2
+%% Purpose: One of the states. 
+%% Args: processID of the client,processID of the storage
+%%--------------------------------------------------------------------
+
 busy(ClientPid, StoragePid) ->
     receive
 	{received, ClientPid} ->
@@ -49,6 +92,11 @@ busy(ClientPid, StoragePid) ->
 	    free(StoragePid)
     end.
 
+%%--------------------------------------------------------------------
+%% Function:terminate/1
+%% Purpose: kill the client when the mutex shuts down
+%% Args: processID of the storage
+%%--------------------------------------------------------------------
 terminate(StoragePid) ->
     receive
 	{_Function, [_Args], ClientPid } ->
