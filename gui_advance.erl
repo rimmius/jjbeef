de1d37132f8f16fd3b2fc49f36c8b3a67588d066
unknown <Jody@.(none)>
2011-12-16 14:35:38 +0100

    updated gui_advance

--- gui_advance.erl
+++ gui_advance.erl
@@ -5,12 +5,12 @@
 %%%--------------------------------------------------------------------- 
 %%% Description module gui_advance
 %%%--------------------------------------------------------------------- 
-%%% What this module does...
+%%% This module handles the advance features of the GUI.
 %%%--------------------------------------------------------------------- 
 %%% Exports 
 %%%--------------------------------------------------------------------- 
 %%% start()
-%%%   what it does....
+%%%   Start() begins the create_advance_feature()
 %%%--------------------------------------------------------------------- 
 
 -module(gui_advance).
@@ -24,9 +24,11 @@ start(MainPanel, OuterSizer) ->
 
 %%--------------------------------------------------------------------
 %% Function: create_advance_feature/2
-%% Purpose: 
-%% Args: 
-%% Returns:
+%% Purpose:  which handles the
+%%  declaration of  widgets 
+%%
+%% Args: MainPanel, OuterSizer
+%% Returns: CreateText1104, CompleteText1105,  HashText1106 
 %%--------------------------------------------------------------------
 
 create_advance_feature(MainPanel, OuterSizer) ->  
@@ -36,25 +38,25 @@ create_advance_feature(MainPanel, OuterSizer) ->
     wxImageList:add(IL, Peers),
     wxNotebook:assignImageList(Notebook, IL),
     
-   {CreateText1104, CompleteText1105,  HashText1106} = file_info_page(Notebook),
-    TorNameText1201 = torrent_info_page(Notebook),
-    peer_page(Notebook), 
+   {CreateText1104, CompleteText1105,  HashText1106, TSizeText1103} = file_info_page(Notebook),
+    {TorNameText1201, TpText1204, CpText1205} = torrent_info_page(Notebook),
+   
       
     wxSizer:add(OuterSizer, Notebook, [{proportion, 0}, {flag, ?wxEXPAND}]),
     {TorNameText1201, CreateText1104, CompleteText1105, HashText1106}.
 
 %%--------------------------------------------------------------------
 %% Function: file_info_page/1
-%% Purpose: 
-%% Args: 
-%% Returns:
+%% Purpose: This handles declaration variables, and setting of the sizing 
+%% framework for the page for information on the file info.
+%% Args: Notebook
+%% Returns: CreateText1104, CompleteText1105,  HashText1106
 %%--------------------------------------------------------------------
 
 file_info_page(Notebook) ->
     Fpage = wxPanel:new(Notebook, []),
-        FPathText1101 = wxStaticText:new(Fpage, 1101, "Filepath: ", []),
-    PText1102 = wxStaticText:new(Fpage, 1102, "Pieces: 4 of 23", []),
-    TSizeText1103 = wxStaticText:new(Fpage, 1103, "TotalSize: 65.7MB", []),
+    FPathText1101 = wxStaticText:new(Fpage, 1101, "Filesize(in bits: ", []),
+    TSizeText1103 = wxStaticText:new(Fpage, 1103, " ", []),
     CreateText1104 = wxStaticText:new(Fpage, 1104, "Created on: ", []),
     CompleteText1105 = wxStaticText:new(Fpage, 1105, "Completed on: ", []),
     HashText1106 = wxStaticText:new(Fpage, 1106, "Hash: ", []),
@@ -67,8 +69,7 @@ file_info_page(Notebook) ->
     
     wxSizer:add(FileGridSizer, FPathText1101, [{proportion, 0}, 
 						{flag, ?wxALIGN_LEFT}]),
-    wxSizer:add(FileGridSizer, PText1102, [{proportion, 0}, 
-					     {flag, ?wxALIGN_RIGHT}]),
+    wxSizer:add(FileGridSizer, 30, 10, []),
     wxSizer:add(FileGridSizer,  TSizeText1103, [{proportion, 0}, 
 						  {flag, ?wxALIGN_LEFT}]), 
     wxSizer:add(FileGridSizer, 30, 10, []),
@@ -84,13 +85,14 @@ file_info_page(Notebook) ->
     wxPanel:setSizer(Fpage, FileBoxSizer),
     wxSizer:layout(FileBoxSizer),
     wxNotebook:addPage(Notebook, Fpage, "File Info", []),
-    {CreateText1104, CompleteText1105,  HashText1106}.
+    {CreateText1104, CompleteText1105,  HashText1106, TSizeText1103 }.
 
 %%--------------------------------------------------------------------
 %% Function: torrent_info_page/1
-%% Purpose: 
-%% Args: 
-%% Returns:
+%% Purpose: The handling the torrent info in regards to declaration, 
+%%            framework of the structure of the torrent info page
+%% Args: Notebook
+%% Returns:  TorNameText1201, 
 %%--------------------------------------------------------------------
 
 torrent_info_page(Notebook) ->
@@ -98,8 +100,8 @@ torrent_info_page(Notebook) ->
     Tpage = wxPanel:new(Notebook, []),
 
     TorNameText1201 = wxStaticText:new(Tpage, 1201, "Torrent Name: ", []),
-    SeedText1202 = wxStaticText:new(Tpage, 1202, "Seeders: 125", []),
-    LeechText1203  = wxStaticText:new(Tpage, 1203, "Leechers: 12", []),
+    SeedText1202 = wxStaticText:new(Tpage, 1202, "Seeders: ", []),
+  
     TpText1204 = wxStaticText:new(Tpage, 1204, "Total Pieces:", []), 
     CpText1205 = wxStaticText:new(Tpage, 1205, 
 				  "Current number of pieces: ", []),
@@ -113,7 +115,7 @@ torrent_info_page(Notebook) ->
     wxSizer:add(TorrentGridSizer, 30, 10, []),
     wxSizer:add(TorrentGridSizer, SeedText1202, [{proportion, 0}, 
 						 {flag, ?wxALIGN_LEFT}]),
-    wxSizer:add(TorrentGridSizer, LeechText1203, [{proportion, 0}, 
+    wxSizer:add(TorrentGridSizer, 30,10, [{proportion, 0}, 
 						  {flag, ?wxALIGN_RIGHT}]), 
     wxSizer:add(TorrentGridSizer, TpText1204, [{proportion, 0}, 
 					       {flag, ?wxALIGN_LEFT}]),
@@ -122,25 +124,7 @@ torrent_info_page(Notebook) ->
 
     wxPanel:setSizer(Tpage, TorrentBoxSizer ),
     wxNotebook:addPage(Notebook, Tpage, "Torrent Info", []),
-    TorNameText1201.
+   {TorNameText1201, TpText1204, CpText1205}.
     
-%%--------------------------------------------------------------------
-%% Function: peer_page/1
-%% Purpose: 
-%% Args: 
-%% Returns:
-%%--------------------------------------------------------------------
 
-peer_page(Notebook) ->
-    Page3 = wxPanel:new(Notebook, []),
-    wxPanel:setBackgroundColour(Page3, ?wxCYAN),
-    PageText3 = wxStaticText:new(Page3, ?wxID_ANY, 
-				 "This is a Torrent Info tab.",
-				[{pos, {50, 100}}]),
-    wxStaticText:setForegroundColour(PageText3, ?wxBLACK),
-    PageSizer3 = wxBoxSizer:new(?wxHORIZONTAL),
-    wxSizer:add(PageSizer3, PageText3),
-    wxPanel:setSizer(Page3, PageSizer3),
-    wxNotebook:addPage(Notebook, Page3, "Peers", []),
-    wxNotebook:setPageImage(Notebook, 2, 0).
     
