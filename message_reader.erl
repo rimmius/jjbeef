80357cbd22f960530d6be764a494c6cae783b5a8
eva-lisa <guskedev@student.gu.se>
2011-12-16 11:24:50 +0100

    header changed

--- message_reader.erl
+++ message_reader.erl
@@ -1,12 +1,23 @@
-%%%-------------------------------------------------------------------
-%%% @author  <Bruce@THINKPAD>
-%%% @copyright (C) 2011, 
-%%% @doc
+%%%---------------------------------------------------------------------
+%%% Created by: Bruce Yinhe
+%%% Creation date: 2011-11-27
+%%%--------------------------------------------------------------------- 
+%%% Description module message_reader
+%%%--------------------------------------------------------------------- 
 %%% This module takes the variables from the message and processes them
-%%%
-%%% @end
-%%% Created : 16 Dec 2011 by  <Bruce@THINKPAD>
-%%%-------------------------------------------------------------------
+%%%--------------------------------------------------------------------- 
+%%% Exports 
+%%%--------------------------------------------------------------------- 
+%%% start(Requester_pid, Uploader_pid, Peer_mutex_pid, 
+%%%          Piece_mutex_pid, File_storage_pid, Peer_id)
+%%%   starts the process
+%%%   returns  Pid
+%%%--------------------------------------------------------------------- 
+%%% read_msg(Pid, Type, Args)
+%%%   sends the variables to the loop
+%%%   returns Message
+%%%---------------------------------------------------------------------
+
 -module(message_reader).
 
 %% API
@@ -19,24 +30,11 @@
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc
-%% starts the process
-%%
-%% @spec start(Requester_pid, Uploader_pid, Peer_mutex_pid, 
-%% Piece_mutex_pid, File_storage_pid, Peer_id) -> Pid
-%% @end
-%%--------------------------------------------------------------------
-start(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_pid, Peer_id) ->
-    spawn(?MODULE, loop, [Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_pid, Peer_id]).
+start(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, 
+      File_storage_pid, Peer_id) ->
+    spawn(?MODULE, loop, [Requester_pid, Uploader_pid, Peer_mutex_pid, 
+			  Piece_mutex_pid, File_storage_pid, Peer_id]).
 
-%%--------------------------------------------------------------------
-%% @doc
-%% sends the variables to the loop
-%%
-%% @spec read_msg(Pid, Type, Args) -> Message
-%% @end
-%%--------------------------------------------------------------------
 read_msg(Pid, Type, Args) ->
     Pid ! {Type, Args}.
 
@@ -45,40 +43,49 @@ read_msg(Pid, Type, Args) ->
 %%%===================================================================
 
 %%--------------------------------------------------------------------
-%% @doc
-%% the loop
-%%
-%% @spec loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_pid, Peer_id) -> void()
-%% @end
+%% Function: loop/1
+%% Purpose: the loop
+%% Args: Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, 
+%%       File_storage_pid, Peer_id
 %%--------------------------------------------------------------------
-loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_pid, Peer_id) ->
+
+loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, 
+     File_storage_pid, Peer_id) ->
     receive
 	{keep_alive, []} ->
 	    piece_requester:send_event(Requester_pid, keep_alive, ok);
 	{bitfield, [Bitfield, Bitfield_len]} ->
-	    Bitfield_in_list = make_bitfield_with_index(<<Bitfield:Bitfield_len>>, 0),
+	    Bitfield_in_list = 
+		make_bitfield_with_index(<<Bitfield:Bitfield_len>>, 0),
 	    %% update piece storage
-	    mutex:request(Piece_mutex_pid, insert_bitfield, [Peer_id, Bitfield_in_list]),
+	    mutex:request(Piece_mutex_pid, insert_bitfield, 
+			  [Peer_id, Bitfield_in_list]),
 	    mutex:received(Piece_mutex_pid),
 	    
 	    %% get interest from file storage
-	    {ok, List_of_interest} = mutex:request(File_storage_pid, compare_bitfield, [Bitfield_in_list]),
+	    {ok, List_of_interest} = mutex:request(File_storage_pid, 
+						   compare_bitfield, 
+						   [Bitfield_in_list]),
 	    mutex:received(File_storage_pid),
 	    
 	    %% save in fsm bitfield and interest
-	    piece_requester:update_interest(Requester_pid, List_of_interest, add);	   
+	    piece_requester:update_interest(Requester_pid, List_of_interest, 
+					    add);	   
 	{have, [Piece_index]} ->
 	    %% update piece storage
-	    mutex:request(Piece_mutex_pid, update_bitfield, [Peer_id, Piece_index]),
+	    mutex:request(Piece_mutex_pid, update_bitfield, 
+			  [Peer_id, Piece_index]),
 	    mutex:received(Piece_mutex_pid),
 
 	    %% get interest from file storage
-	    Am_interested = mutex:request(File_storage_pid, have, [Piece_index]),
+	    Am_interested = mutex:request(File_storage_pid, have, 
+					  [Piece_index]),
 	    mutex:received(File_storage_pid),
 
 	    case Am_interested of
 		true -> 
-		    piece_requester:update_interest(Requester_pid, [Piece_index], add);
+		    piece_requester:update_interest(Requester_pid, 
+						    [Piece_index], add);
 		false -> ok 
 	    end;
 	{am_choked, [Arg]} ->
@@ -88,24 +95,29 @@ loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_
 	{port, [_Listen_port]} ->	    
 	    ok;
 	{piece, [Index, Begin, Block, Block_len]} ->
-	    Is_complete = mutex:request(File_storage_pid, insert_chunk, [Requester_pid, Index, Begin, Block, Block_len]),
+	    Is_complete = mutex:request(File_storage_pid, insert_chunk, 
+					[Requester_pid, Index, Begin, Block, 
+					 Block_len]),
 	    mutex:received(File_storage_pid),
-	    piece_requester:send_event(Requester_pid, piece, {Is_complete, Index});
+	    piece_requester:send_event(Requester_pid, piece, {Is_complete, 
+							      Index});
 	{request, [Index, Begin, Length]} ->
-	    piece_uploader:send_event(Uploader_pid, request, [Index, Begin, Length]);
+	    piece_uploader:send_event(Uploader_pid, request, [Index, Begin, 
+							      Length]);
 	{cancel, [Index, Begin, Length]} ->
-	    piece_uploader:send_event(Uploader_pid, cancel, [Index, Begin, Length])
+	    piece_uploader:send_event(Uploader_pid, cancel, [Index, Begin, 
+							     Length])
     end,
-    loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, File_storage_pid, Peer_id).
+    loop(Requester_pid, Uploader_pid, Peer_mutex_pid, Piece_mutex_pid, 
+	 File_storage_pid, Peer_id).
 
 %%--------------------------------------------------------------------
-%% @doc
-%% take a bitfield in bitstring, return a bitfield (with index) in list 
-%% [{0,0}, {0,1}, {0,2}, ... ]
-%%
-%% @spec make_bitfield_with_index(Bitfield_in_bin, Index) ->
-%% @end
+%% Function: make_bitfield_with_index/2
+%% Purpose: take a bitfield in bitstring, return a bitfield (with index) 
+%% in list [{0,0}, {0,1}, {0,2}, ... ]
+%% Args: Bitfield_in_bin, Index
 %%--------------------------------------------------------------------
+
 make_bitfield_with_index(<<P:1>>, Index) ->
     [{P, Index}];
 make_bitfield_with_index(<<P:1, Rest/bits>>, Index) ->
